5679b63437d4c8863f90e0f17335af97f4cef79c
matmang <64837797+matmang@users.noreply.github.com>
2021-08-21 16:28:44 +0900

    user sampledata create

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
 
+@admin.register(models.User)
+class UserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile", {"fields": ("avatar", "phone_number", "half_money")},),
+    )
+
+    list_display = UserAdmin.list_display + ("half_money",)
+
 # Register your models here.
--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 class User(AbstractUser):
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
-    phone_number = models.IntegerField(default=None)
-    birthday = models.DateField(default=None)
-    half_money = models.DecimalField(max_digits=9, decimal_places=1)
+    phone_number = models.IntegerField(default=0)
+    birthday = models.DateField(default=now())
+    half_money = models.DecimalField(max_digits=9, decimal_places=1, default=0)
 # Create your models here.
