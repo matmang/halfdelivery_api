03e37c1db33a302f4faf2745dfb1703d9b3e0845
matmang <64837797+matmang@users.noreply.github.com>
2021-08-29 23:10:55 +0900

    오류 삭제

--- users/models.py
+++ users/models.py
@@ -7,5 +7,4 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=13);
     birthday = models.DateField(auto_now_add=True)
     half_money = models.DecimalField(max_digits=9, decimal_places=1, default=0)
-    auth_number = models.CharField('인증번호', max_length=30);
 # Create your models here.
